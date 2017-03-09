71ad91c501093c82a8dd634abc5c923eeaf76766
Gabriel Burnworth <gabrielburnworth@users.noreply.github.com>
2017-03-09 09:55:38 -0800

    comma

--- take_photo.py
+++ take_photo.py
@@ -64,7 +64,7 @@ def rpi_camera_photo():
     'Take a photo using the Raspberry Pi Camera.'
     from subprocess import call
     try:
-        retcode = call(["raspistill", "-w", "640", "-h", "480" "-o", image_filename()])
+        retcode = call(["raspistill", "-w", "640", "-h", "480", "-o", image_filename()])
         if retcode == 0:
             print("Image saved: {}".format(image_filename()))
         else:
