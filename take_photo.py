7a06749fe6bd7e91db0261e6aa3fdfc0770c0b43
Gabriel Burnworth <gabrielburnworth@users.noreply.github.com>
2017-09-14 12:23:38 -0700

    no mode

--- take_photo.py
+++ take_photo.py
@@ -99,7 +99,7 @@ def rpi_camera_photo():
     try:
         filename = image_filename()
         retcode = call(
-            ["raspistill", "-md", "2", "-o", tmp_path(filename)])
+            ["raspistill", "-o", tmp_path(filename)])
         move(tmp_path(filename), upload_path(filename))
         if retcode == 0:
             print("Image saved: {}".format(upload_path(filename)))
